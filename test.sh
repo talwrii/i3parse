61b0a9a8e03361f6b1dcffdd44133bfed83cb963
Tal Wrii <talwrii@gmail.com>
2017-11-14 02:05:37 +0100

    Add some basic testing

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+here="$(dirname ${BASH_SOURCE[0]})"
+
+cd "$here"
+
+i3parse bindings /etc/i3/config > /dev/null
+rm -rf venv
+virtualenv venv
+venv/bin/pip install .
+venv/bin/i3parse --help > /dev/null
